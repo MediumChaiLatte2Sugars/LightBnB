cdab3228fe2b12397950757e70ebfcec748bee11
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-02-23 15:56:56 -0500

    Refactor: use node-pg suggested structure

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,14 +1,6 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
-const { Pool } = require('pg');
-require('dotenv').config();
-
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  database: process.env.DATABASE
-});
+const pool = require('../db');
 
 /// Users
 
